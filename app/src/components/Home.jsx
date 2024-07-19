2f36812ae3785d433ed31f1c3ff99a42028dc1d0
joaonunez <joaovaldiglesias@gmail.com>
2024-07-19 18:18:10 -0400

    empezando de 0

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -1,53 +1,7 @@
-import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React,{useState, useEffect} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
-import tareas from "../scripts/tareas";
 
-const apiUrl = 'https://playground.4geeks.com/todo/users/joaodev';
-export const Home = () => {
-  const [inputValue, setInputValue] = useState("");
-  const [tareasArray, setTareas] = useState(tareas);
+//url de mi api creada desde 4geeks
 
-  const borrarTarea = (index) => {
-    const filtroTareas = tareasArray.filter((_, i) => i !== index);
-    setTareas(filtroTareas);
-  };
-
-  return (
-    <>
-      <div className="container">
-        <h1>Gestión De Tareas</h1>
-        <ul>
-          <li>
-            <input
-              type="text"
-              placeholder="Ingresa Una Nueva Tarea"
-              onChange={(escrito) => setInputValue(escrito.target.value)}
-              value={inputValue}
-              onKeyDownCapture={(tecla) => {
-                if (tecla.key === "Enter") {
-                  setTareas(tareasArray.concat(inputValue));
-                  
-                  setInputValue("");
-                }
-                
-              }}
-            ></input>
-          </li>
-          {tareasArray.map((tarea, index) => (
-            <li key={index}>
-              {tarea}
-              <FontAwesomeIcon
-                icon={faTrash}
-                className="fa-trash"
-                onClick={() => borrarTarea(index)}
-              />
-            </li>
-            
-          ))}
-        </ul>
-        <div>{tareasArray.length} Tareas.</div>
-      </div>
-    </>
-  );
-};
+const apiUrl = ''
