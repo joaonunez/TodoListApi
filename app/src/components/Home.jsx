36af2604018bdbb85e26a8958fd44f06c11b8a86
joaonunez <joaovaldiglesias@gmail.com>
2024-07-21 15:46:33 -0400

    GET listo

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -1,7 +1,61 @@
-import React,{useState, useEffect} from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
-//url de mi api creada desde 4geeks
 
-const apiUrl = ''
+
+export const Home = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [tareasArray, setTareas] = useState([]);
+
+ 
+  const getTareas = () =>{
+    fetch("https://playground.4geeks.com/todo/users/joaodev",{
+      method: "GET",
+      headers:{
+        "Content-Type": "application/json",
+      },
+    })
+    .then((response) => response.json())
+    .then((data) => setTareas(data.todos))
+    .catch((error) => console.log(error));
+  };
+
+  useEffect(()=>{
+    getTareas();
+  },[]);
+
+
+  return (
+    <div className="container">
+      <h1>Gestión De Tareas</h1>
+      <ul>
+        <li>
+          <input
+            type="text"
+            placeholder="Ingresa Una Nueva Tarea"
+            onChange={(escrito) => setInputValue(escrito.target.value)}
+            value={inputValue}
+            onKeyDownCapture={(tecla) => {
+              if (tecla.key === "Enter") {
+                
+                setInputValue("");
+              }
+            }}
+          ></input>
+        </li>
+        {tareasArray.map((tarea, index) => (
+          <li key={tarea.id}>
+            {tarea.label}
+            <FontAwesomeIcon
+              icon={faTrash}
+              className="fa-trash"
+              
+            />
+          </li>
+        ))}
+      </ul>
+      <div>{tareasArray.length} Tareas.</div>
+    </div>
+  );
+};
